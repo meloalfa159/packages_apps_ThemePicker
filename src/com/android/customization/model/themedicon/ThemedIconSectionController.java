89ec0792176dc39186c7f915634ff89ea955be27
jhonboy121 <alfredmathew05@gmail.com>
2022-04-11 02:07:34 +0530

    ThemePicker: add support for custom themed icon pack [2/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/customization/model/themedicon/ThemedIconSectionController.java
+++ src/com/android/customization/model/themedicon/ThemedIconSectionController.java
@@ -64,7 +64,7 @@ public class ThemedIconSectionController implements
 
     @Override
     public boolean isAvailable(@Nullable Context context) {
-        return context != null && mThemedIconOptionsProvider.isThemedIconAvailable();
+        return false;
     }
 
     @Override
